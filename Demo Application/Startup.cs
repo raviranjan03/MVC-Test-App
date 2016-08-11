f48d2f3974adbd203289f47e000fe7cf2880d454
SAPIENT\rran15 <rranjan15@sapient.com>
2016-08-11 15:56:17 +0530

    Checking in the code

---
+++ Demo Application/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Demo_Application.Startup))]
+namespace Demo_Application
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
